322c635eaf1cf87af029b751f14636fdc60709de
Christian Todd <christiantodd27@icloud.com>
2017-04-27 13:21:59 -0400

    define get routes and add morgan logging.

--- server.js
+++ server.js
@@ -1,13 +1,84 @@
 const express = require('express');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
+const morgan = require('morgan');
 
 const { PORT, DATABASE_URL } = require('./config.js');
 const { BlogPost } = require('./models');
 
 const app = express();
-app.use(bodyParser.json());
 
-// init
+app.use(bodyParser.json());
+app.use(morgan('common'));
+
+mongoose.Promise = global.Promise;
+
+// Get all posts
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find()
+    .exec()
+    .then(posts => res.json(posts.map(post => post.apiRepresentation())))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'Internal server error'});
+    });
+});
+
+// Get single posts by id
+app.get('/posts/:id', (req, res) => {
+  BlogPost
+    .findById(req.params.id)
+    .exec()
+    .then(post => res.json(post.apiRepresentation()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ERROR: 'INTERNAL SERVER ERROR'});
+    });
+});
+
+// CATCH ALL ROUTE
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+let runServer = (databaseUrl = DATABASE_URL, port = PORT) => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+          console.log(`Your app is listening on port ${port} with databaseurl ${databaseUrl}.`);
+          resolve();
+        })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+let closeServer = () => {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server. Goodbye old friend.');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+
+if (require.main === module){
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { runServer, closeServer, app };

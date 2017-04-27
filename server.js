7b107e75bc56c1ba9cfe1e4fc187b7083284887c
Christian Todd <christiantodd27@icloud.com>
2017-04-27 16:07:56 -0400

    complete routes.

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/posts', (req, res) => {
     .then(posts => res.json(posts.map(post => post.apiRepresentation())))
     .catch(err => {
       console.error(err);
-      res.status(500).json({error: 'Internal server error'});
+      res.status(500).json({
+        error: 'Internal server error'
+      });
     });
 });
 
@@ -33,36 +35,97 @@ app.get('/posts/:id', (req, res) => {
     .then(post => res.json(post.apiRepresentation()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ERROR: 'INTERNAL SERVER ERROR'});
+      res.status(500).json({
+        ERROR: 'INTERNAL SERVER ERROR'
+      });
     });
 });
 
 app.post('/posts', (req, res) => {
   const reqFields = ['title', 'content', 'author'];
   reqFields.forEach(field => {
-    if (!(field in req.body)){
+    if (!(field in req.body)) {
       const message = `Missing ${field} in the request body.`;
       console.error(message);
       return res.status(400).send(message);
     }
   });
-  
+
   BlogPost
     .create({
       title: req.body.title,
       content: req.body.content,
-      author: req.body.author
+      author: {
+        firstName: req.body.author.firstName,
+        lastName: req.body.author.lastName
+      }
     })
     .then(blogPost => res.status(201).json(blogPost.apiRepresentation()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ERROR: 'INTERNAL SERVER ERROR. SEEK HELP'});
+      res.status(500).json({
+        ERROR: 'INTERNAL SERVER ERROR. SEEK HELP'
+      });
     });
 });
 
+// delete post by id request params
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      let message = `Successfully deleted blog with post id of ${req.params.id}.`
+      console.log(message);
+      res.json({message: message});
+      res.status(204).end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        ERROR: 'INTERNAL SERVER ERROR. CALL 911'
+      });
+    });
+});
+
+// update post by id request parameter
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id === req.body.id)) {
+    res.status(400).json({
+      ERROR: `Request path id and request body id values must match`
+    });
+  }
+
+  const updated = {};
+  const updatableFields = ['title', 'content', 'author'];
+
+  updatableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  BlogPost
+    .findByIdAndUpdate(req.params.id, {
+      $set: updated
+    }, {
+      new: true
+    })
+    .exec()
+    .then(updatedPost => res.status(201).json(updatedPost.apiRepresentation()))
+    .catch(err => {
+      res.status(500).json({
+        ERROR: 'INTERNAL SERVER ERROR. IT BLEW UP'
+      })
+    });
+})
+
+
 // CATCH ALL ROUTE
-app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
+app.use('*', function (req, res) {
+  res.status(404).json({
+    message: 'Not Found'
+  });
 });
 
 let server;
@@ -100,7 +163,7 @@ let closeServer = () => {
 }
 
 
-if (require.main === module){
+if (require.main === module) {
   runServer().catch(err => console.error(err));
 }
 
